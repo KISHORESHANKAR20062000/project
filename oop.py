8085e7b2340aa740a75ae7688fc2993fecf6fb35
KISHORESHANKAR20062000 <60849170+KISHORESHANKAR20062000@users.noreply.github.com>
2020-12-26 17:46:17 +0530

    Added hello file

--- oop.py
+++ oop.py
@@ -3,38 +3,44 @@ class student:
         self.name = name
         self.age = age
         self.grade = grade
-        
+
     def get_name(self):
         return self.name
+
     def get_age(self, age):
         return self.age
+
     def get_grade(self, grade):
         return self.grade
+
+
 class course:
     def __init__(self, subname, max_student):
         self.subname = subname
         self.max_student = max_student
         self.students = []
-        
-    def add_students(self,student):
+
+    def add_students(self, student):
         if len(self.students) < self.max_student:
             self.students.append(student)
             print(" added to")
             return True
         else:
             print("Course full")
+
     def calculate_average(self):
         value = 0
         for value in self.students:
             value += self.get_grade()
-        return value/len(self.students)
+        return value / len(self.students)
 
 
 s1 = student("kishore", 20, 50)
 s2 = student("guru", 20, 20)
 s3 = student("karthi", 20, 75)
-s3 = student("vignesh", 28, 75)
-s3 = student("bala", 26, 75)
+s6 = student("vignesh", 28, 75)
+s5 = student("bala", 26, 75)
+s4 = student("ARUN", 27, 95)
 Course = course('science', 2)
 Course.add_students(s1)
 Course.add_students(s2)
